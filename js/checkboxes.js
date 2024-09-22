a4d451ef3a10a0d0cae7d6a3b8ba6f1b6a76eeeb
Ruben Bensimon <rubensbensimon@Gmail.com>
2024-09-22 22:24:51 +0300

    js chkbx

--- js/checkboxes.js
+++ js/checkboxes.js
@@ -1,10 +1,14 @@
 var present = document.getElementById('presence');
 var absent = document.getElementById('absence');
 
-if (present.checked) {
-  document.getElementById('absence').checked = false;
-}
+present.addEventListener('change', function() {
+    if (present.checked) {
+        document.getElementById('absence').checked = false;
+    }
+});
 
-if (absent.checked) {
-  document.getElementById('presence').checked = false;
-}
+absent.addEventListener('change', function() {
+    if (absent.checked) {
+      document.getElementById('presence').checked = false;
+    }
+});
