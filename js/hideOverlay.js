d3a0d5bb8d5914f8dc41a5537b2a2355302fa34f
Ruben Bensimon <rubensbensimon@Gmail.com>
2024-09-23 22:00:25 +0300

    arrow slide

--- js/hideOverlay.js
+++ js/hideOverlay.js
@@ -1,6 +1,13 @@
+// Fonction qui déclenche l'animation et retire l'overlay
 function hideOverlay() {
-    console.log("hideOverlay called");
-    var overlay = document.getElementById('overlay');
-    overlay.style.display = "none";
-    document.body.classList.remove("no-scroll");
+    const btnLink = document.querySelector('.btnLink');
+    
+    // Ajoute la classe 'animate' pour déclencher l'animation de la flèche
+    btnLink.classList.add('animate');
+    
+    // Attends la fin de l'animation avant de retirer l'overlay
+    setTimeout(() => {
+        document.getElementById('overlay').style.display = 'none';
+        document.body.classList.remove('no-scroll'); // Permet de scroller après avoir caché l'overlay
+    }, 250); // Correspond à la durée de l'animation (0.5s)
 }
